c58731506f024357b71fa82f1e24caa568f066ad
jorgitodf <jspaiva.1977@gmail.com>
2017-02-08 17:02:31 -0200

    Bd: em 08/02/2017 às 17h00

--- core/Model.php
+++ core/Model.php
@@ -3,18 +3,18 @@
 class Model {
 
     protected $db;
-  
+
     public function __construct() {
         global $config;
-		try {
-			$this->db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
-			$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		} catch (PDOException $ex) {
-			if ($ex->getCode() == 1049) {
-				echo "O Banco de Dados <b>".$config['dbname']."</b> não Existe...";
-				exit;
-			}
-		}
+        try {
+            $this->db = new PDO("mysql:dbname=" . $config['dbname'] . ";host=" . $config['host'], $config['dbuser'], $config['dbpass']);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $ex) {
+            if ($ex->getCode() == 1049) {
+                echo "O Banco de Dados <b>" . $config['dbname'] . "</b> não Existe...";
+                exit;
+            }
+        }
     }
 
 }
