2b45a1ca4f9eb4eda7e90e0e6b8bb1dda16f8772
jorgitodf <jspaiva.1977@gmail.com>
2017-09-24 23:25:59 -0300

    BD de 24/09/2017

--- config.php
+++ config.php
@@ -28,5 +28,12 @@ if (ENVIRONMENT == 'development' && $hostname == 'Jorgito') {
     $config['dbpass'] = 'root';
 	$config['ambiente'] = ENVIRONMENT;
 	$config['hostname'] = $hostname;
+} else if (ENVIRONMENT == 'development' && $hostname == 'JORGITO-VM') {
+	$config['dbname'] = 'conta';
+    $config['host'] = 'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = 'Camelo69';
+	$config['ambiente'] = ENVIRONMENT;
+	$config['hostname'] = $hostname;
 }
 
